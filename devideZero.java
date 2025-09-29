bcee9bed8f75d625c642f7bf4fd63b68bae59dec
nayansurve <survenayan870@gmail.com>
2025-09-29 15:19:39 +0530

    updatedfileofjava

---
+++ devideZero.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+import java.util.*;
+
+public class  devideZero
+
+{
+    public static void main (String args[])
+    {
+
+  
+
+    double a=10.0;
+    double b=0.0;
+    double c=a/b;
+    System.out.println("C="+c);
+      }
+}
+
+
